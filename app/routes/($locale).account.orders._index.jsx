0d8f5682955e0b86d6f5369f23769c88c1b154cf
vickyborderx <vicky@borderxai.com>
2025-02-20 14:04:00 -0800

    edit profile

--- app/routes/($locale).account.orders._index.jsx
+++ app/routes/($locale).account.orders._index.jsx
@@ -73,7 +73,7 @@ function EmptyOrders() {
       <p>You haven&apos;t placed any orders yet.</p>
       <br />
       <p>
-        <Link to="/collections">Start Shopping →</Link>
+        <Link to="/">Start Shopping →</Link>
       </p>
     </div>
   );
