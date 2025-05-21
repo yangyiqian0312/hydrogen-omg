701c06a0e7a1bc48cf4428bb682897fa2a7f3da5
Carrie Wang <carrie@borderxai.com>
2025-05-20 17:36:36 -0700

    add 3 men brands

--- app/routes/($locale).products.men.jsx
+++ app/routes/($locale).products.men.jsx
@@ -103,7 +103,7 @@ const Men = (selectedVariant) => {
 
 
   const [isOpen, setIsOpen] = useState(false);
-  const brands = ['Versace','Bvlgari', 'Burberry', 'GUCCI','Givenchy','Tiffany','Lattafa','Giorgio Armani', 'Valentino ', 'Viktor & Rolf', 'YSL'];
+  const brands = ['Versace','Carolina Herrera','Jean Paul Gaultier','Prada','Bvlgari', 'Burberry', 'GUCCI','Givenchy','Tiffany','Lattafa','Giorgio Armani', 'Valentino ', 'Viktor & Rolf', 'YSL'];
   const [selectedBrand, setSelectedBrand] = useState(null);
 
 
